707a67299bba6b78e9d1553e84190242cb4cb5c7
Arvind <arvindrtod24@gmail.com>
2024-01-15 22:50:25 +0530

    added packages

---
+++ src/main/java/com/NetworkMarketing/NetworkMarketing/NetworkMarketingApplication.java
@@ -0,0 +1,13 @@
+package com.NetworkMarketing.NetworkMarketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkMarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkMarketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NetworkMarketing/NetworkMarketing/NetworkMarketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NetworkMarketing.NetworkMarketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkMarketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
